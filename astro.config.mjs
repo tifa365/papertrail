7b9cd01f6f46c32497c718d3df3afe518b9f462c
tim <tfangmeyer@gmail.com>
2025-11-03 20:41:07 +0100

    Configure Astro base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://tifa365.github.io',
+  base: '/papertrail',
+});
